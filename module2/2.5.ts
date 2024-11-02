dc5ae974ca38f3eee30b3ff1c830ee7dc81889fc
Rakesh Biswas <rbiswas01999@gmail.com>
2024-11-02 23:46:39 +0600

    2.4.ts - (function with generics) added.

--- module2/2.5.ts
+++ module2/2.5.ts
@@ -12,8 +12,13 @@
 
     const res1 = createArray('Bangladesh')
     const resGeneric = createArrayWithGeneric<string>('Bangladesh')
-    // const resGenericObj = createArrayWithGeneric<id:number>({id: 222, name: 'Mr, Pashan'})
-    const resGenericObj = createArrayWithGeneric<{ id: number; name: string }>({ id: 222, name: 'Mr, Pashan' })
+
+    type User = { id: number; name: string };
+
+    const resGenericObj = createArrayWithGeneric<User>({
+        id: 222,
+        name: 'Mr, Pashan'
+    })
 
     // 
 }
