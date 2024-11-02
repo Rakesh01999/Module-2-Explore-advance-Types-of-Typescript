0bd2bcd06ffcb6c5803e599dc0346a850a277614
Rakesh Biswas <rbiswas01999@gmail.com>
2024-11-02 23:53:52 +0600

    2.4.ts - adding . .

--- module2/2.5.ts
+++ module2/2.5.ts
@@ -19,6 +19,23 @@
         id: 222,
         name: 'Mr, Pashan'
     })
+    
+    
+
+
+    const createArrayWithTuple = <T, Q>(param1: T, param2:Q) => {
+        return [param1];
+    }
+
+    const res1 = createArray('Bangladesh') 
+    const resGeneric = createArrayWithGeneric<string>('Bangladesh')
+
+    type User = { id: number; name: string };
+
+    const resGenericObj = createArrayWithGeneric<User>({
+        id: 222,
+        name: 'Mr, Pashan'
+    })
 
     // 
 }
