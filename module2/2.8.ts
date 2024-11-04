ff1ef0862f93fe778d809f7657055c32bb83b224
Rakesh Biswas <rbiswas01999@gmail.com>
2024-11-04 20:43:56 +0600

    2.8.ts [promise] - added.

--- module2/2.8.ts
+++ module2/2.8.ts
@@ -2,11 +2,28 @@
     // 
     // promise
 
-    const createPromise = (): Promise<boolean> => {
-        return new Promise<boolean>((resolve, reject) => {
+    type Todo = {
+        id: number;
+        userId: number;
+        title: string;
+        completed: boolean
+    }
+
+    const getTodo = async (): Promise<Todo> => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const data = response.json();
+         return data
+        // console.log(data);
+    }
+    getTodo();
+
+    type Something = { something: 'something' };
+    // simulate
+    const createPromise = (): Promise<Something> => {
+        return new Promise<Something>((resolve, reject) => {
             // const data: string = "Something";
             // const data: string = null;  // error
-            const data: boolean = true;  
+            const data: Something = { something: 'something' };
             if (data) {
                 resolve(data)
             } else {
@@ -16,8 +33,8 @@
     }
 
     // calling create promise function
-    const showData = async (): Promise<boolean> => {
-        const data: boolean = await createPromise();
+    const showData = async (): Promise<Something> => {
+        const data: Something = await createPromise();
         // console.log(data);
         return data;
     };
